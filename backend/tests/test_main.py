5b4328b2f1246a4a2a2f1f83996c883a1cff1d2c
Rafi Nissim <rafi@nissim.co.uk>
2026-05-28 15:05:07 +0100

    feat: General CI improvements [rsn24]

---
+++ backend/tests/test_main.py
@@ -0,0 +1,10 @@
+from app.main import app
+from fastapi.testclient import TestClient
+
+client = TestClient(app)
+
+
+def test_root() -> None:
+    response = client.get("/")
+    assert response.status_code == 200
+    assert response.json() == {"Hello": "World"}
