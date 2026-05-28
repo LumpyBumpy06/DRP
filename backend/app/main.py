315a0a8c617591894ea3bf2d52a2b14e0c0143e9
Rafi Nissim <rafi@nissim.co.uk>
2026-05-28 03:46:00 +0100

    fix: Added missing type signatures to main.py and updated makefile. [rsn24]

--- backend/app/main.py
+++ backend/app/main.py
@@ -4,10 +4,10 @@ app = FastAPI()
 
 
 @app.get("/")
-def read_root():
+def read_root() -> dict:
     return {"Hello": "World"}
 
 
 @app.get("/items/{item_id}")
-def read_item(item_id: int, q: str | None = None):
+def read_item(item_id: int, q: str | None = None) -> dict:
     return {"item_id": item_id, "q": q}
