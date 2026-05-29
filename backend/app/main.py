096496ee5a4e8a210633b6c47518c4a2db84754e
Rafi Nissim <rafi@nissim.co.uk>
2026-05-29 01:37:56 +0100

    fix: Removed trailing /'s for endpoints

--- backend/app/main.py
+++ backend/app/main.py
@@ -37,14 +37,14 @@ async def get_tapped_okay(user_id: int) -> bool:
     return False
 
 
-@app.post("/okay/")
+@app.post("/okay")
 async def tap_okay(user_id: UserId) -> None:
     """Elderly person has tapping they are ok"""
     print(f"{user_id} is okay")
     pass
 
 
-@app.post("/token/")
+@app.post("/token")
 async def update_user_notif_token(user_id: UserId, token: str) -> None:
     """Update database with new user's (firebase) device token for sending notifications"""
     print(f"{user_id} has new token: {token}")
